cc7cf607c648e4255c5b86fc8d44a3fbcfdbfaa8
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2021-08-16 09:40:19 +0100

    Seperate files for categories

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ def move_files_to_destination_folder():
         country_file = os.path.join(country_dir, Hyper.HyrdatedTweetFile)
         csv_input = pd.read_csv(country_file, sep=',', error_bad_lines=False, index_col=False, dtype='unicode')
         Helper.printline(f"Country {i}/{no_countries}. {country} has {len(csv_input)} entries")
-        if len(csv_input) >= Hyper.Threshold:
+
+        #if len(csv_input) >= Hyper.Threshold:
+        if country in Hyper.SelectedCountries:
             data_dir = os.path.join(Hyper.DestDir, country)
             Helper.printline(f"Theshold exceeded, copy {country_dir} to {data_dir}")
             shutil.copytree(country_dir, data_dir)
@@ -55,8 +57,14 @@ def select_columns_for_new_file():
     Helper.printline("     ** Started: Select required columns for the new file")
     file_in = os.path.join(Hyper.DestDir, Hyper.HyrdatedTweetLangFile)
     file_out = os.path.join(Hyper.DestDir, Hyper.OutputTweetLangFile)
-    edit_df = pd.read_csv(file_in, usecols=Hyper.field_names)
-    edit_df.to_csv(file_out, index=False)
+    edit_df = pd.read_csv(file_in, usecols=Hyper.input_field_names)
+    edit_df[Hyper.output_field_names].to_csv(file_out, index=False)
+    file_out = os.path.join(Hyper.DestDir, Hyper.FacemaskTweetLangFile)
+    facemask_df = edit_df.query('is_facemask==True')
+    facemask_df[Hyper.output_field_names].to_csv(file_out, index=False)
+    file_out = os.path.join(Hyper.DestDir, Hyper.LockdownTweetLangFile)
+    lockdown_df = edit_df.query('is_lockdown==True')
+    lockdown_df[Hyper.output_field_names].to_csv(file_out, index=False)
     Helper.printline("     ** Ended: Select required columns for the new file")
 
 if __name__ == "__main__":
